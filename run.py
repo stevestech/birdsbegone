8ed639d1a9cd27aee99e05ef96ba5f4ec86c210a
Francis Baster <francisbaster@gmail.com>
2015-07-22 18:55:31 +1200

    Allow running from interpreter or from python run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from hubMotor import *
 from networking import Networking
 from threading import Thread
+import time
 
 hm = HubMotor(6, 5, 17, 7)
 n = Networking(1234, hm)
@@ -8,3 +9,10 @@ n = Networking(1234, hm)
 networkThread = Thread(target=n.run)
 networkThread.daemon = True
 networkThread.start()
+
+if __name__ == "__main__":
+    # Block so that the program doesn't terminate. Not necessary if running
+    # from the interpreter ("import run" will execute the program and allow
+    # access to member variables while running).
+    while True:
+        time.sleep(1)
